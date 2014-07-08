613f97f803c1505c0fb05ce232e6ac554f2fd3d6
Sean Myers <sean.myers@redhat.com>
2014-07-08 13:16:44 -0400

    Attempting to remove the old image after a pull was silly
    Immediately after pulling, the likelihood that there are containers using the old image
    is around 100%, considering that's the whole point of this app.

--- webdriver_wharf/interactions.py
+++ webdriver_wharf/interactions.py
@@ -205,10 +205,6 @@ def pull(image):
     if pulled_image_id != last_pulled_image_id:
         # TODO: Add a config flag on this so we aren't rudely deleting peoples' images
         #       if they aren't tracking a tag
-        with apierror_squasher():
-            # Clean up old images when the tag moves
-            if last_pulled_image_id:
-                client.remove_image(last_pulled_image_id)
         last_pulled_image_id = pulled_image_id
         logger.info('Pulled image "%s" (docker id: %s)', image, pulled_image_id)
         # flag to indicate pulled image is new
