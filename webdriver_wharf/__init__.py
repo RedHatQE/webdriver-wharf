de6a04b85d7f18ad8ad79093d48d7f827c293d5f
Ronny Pfannschmidt <ronny.pfannschmidt@redhat.com>
2017-05-18 18:26:07 +0200

    the one true warning against docker-py

--- webdriver_wharf/__init__.py
+++ webdriver_wharf/__init__.py
@@ -1,6 +1,21 @@
 import logging
 from threading import RLock
 
+import docker
+
+try:
+    docker.AutoVersionClient
+except AttributeError:
+    pass  # yay no docker_py
+else:
+    raise ImportError("""\
+docker-py installation detect
+
+this breaks the good docker client
+please uninstall both docker-py and docker
+to get a working docker after reinstallation of docker
+""")
+
 lock = RLock()
 
 log_format = '[%(levelname)s] %(name)s %(message)s'
